ab502db60ca38d6991502d42864913b24a188f53
dengyichuan <947834020@qq.com>
2019-10-10 01:18:45 +0800

    add set file

---
+++ contains/set/set.go
@@ -0,0 +1,28 @@
+package set
+
+// Set ... TODO https://github.com/deckarep/golang-set/blob/master/set.go
+type Set interface {
+	Add(i interface{}) bool
+
+	Clear()
+
+	Clone() Set
+
+	Contains(i ...interface{}) bool
+
+	Difference(other Set) Set
+
+	Equal(other Set) bool
+
+	Each(func(interface{}) bool)
+
+	Remove(i interface{})
+
+	String() string
+
+	Union(other Set) Set
+
+	Pop() interface{}
+
+	ToSlice() []interface{}
+}
